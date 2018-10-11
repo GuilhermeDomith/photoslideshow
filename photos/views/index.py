2faa7b08f6b5404bcaa747639d981b200ee45ecc
Guilherme Domith <guilhermedomith@gmail.com>
2018-10-11 10:51:06 -0300

    caminho dos dados de usuário alterado

--- photos/views/index.py
+++ photos/views/index.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from usuario.views import googleauth
 from photoslideshow import settings
@@ -10,7 +10,7 @@ import threading, json, os
 
 URL_LISTAR_ALBUMS = 'https://photoslibrary.googleapis.com/v1/albums'
 URL_LISTAR_MIDIAS = 'https://photoslibrary.googleapis.com/v1/mediaItems:search'
-PATH_IMAGENS = settings.BASE_DIR + '/photos/static/photos/img/'
+PATH_IMAGENS = settings.BASE_DIR + '/photos/static/photos/data_users'
 
 class IndexView(LoginRequiredMixin, generic.ListView):
 	template_name = 'photos/index.html'
@@ -36,36 +36,42 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 
 
 def slideshow(request, num_album):
+	if num_album < 1 or num_album > len(request.session['albuns']):
+		return Http404("Este album não existe.")
+
 	album = request.session['albuns'][num_album - 1]
-	album_key = 'album_'+album['id']
-	path = PATH_IMAGENS+album_key
-
-	if album_key not in request.session:
+	path_album = '{path}/{user}/albuns/{id}'.format(path=PATH_IMAGENS, user=request.user, id=album['id'])
+	print('--->'+path_album)
+	if path_album not in request.session:
 		session = googleauth.get_session(request.user)
-		data_search = dict(
-			pageSize=100,
-			albumId=album['id']
-		)
-		response = session.request('POST', URL_LISTAR_MIDIAS, data=data_search)
-		request.session[album_key] = json.loads(response.text)['mediaItems']
+		response = session.request(
+						'POST', 
+						URL_LISTAR_MIDIAS, 
+						data={'pageSize': 100, 'albumId': album['id']}
+					)
 
-		if not os.path.exists(path):
-			os.mkdir(path)
+		request.session[path_album] = json.loads(response.text)['mediaItems']
+
+		if not os.path.exists(path_album):
+			os.mkdir(path_album)
 	
-		for foto in request.session[album_key]:
-			download_imagem(session, path, foto)
+		for foto_info in request.session[path_album]:
+			download_imagem(session, path_album, foto_info)
 
 	return render(request, 'photos/slideshow.html', {
-		'album_key': album_key,
-		'fotos': request.session[album_key]
-		})
+				'album_id': album['id'],
+				'fotos': request.session[path_album]
+		   })
 
 
-def download_imagem(session, path, foto_info):
-	response = session.get(foto_info['baseUrl'] + '=d')
+def download_imagem(session_photos, path, foto_info):
+	response = session_photos.get('{url}=d'.format(url=foto_info['baseUrl']))
 
 	try:
-		with open(path+'/'+foto_info['filename'], 'wb') as foto:
-			foto.write(response.content)
+		path_foto = '{path}/{file_name}'.format(path=path, file_name=foto_info['filename'])
+
+		if not os.path.exists(path_foto):
+			with open(path_foto, 'wb') as foto:
+				foto.write(response.content)
 	except Exception as e:
 		print(e+'\n'+foto_info)

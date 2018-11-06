3736a8310d666f5b4efbad51e36555be318aa30d
Guilherme Domith <guilhermedomith@gmail.com>
2018-11-06 02:32:18 -0200

    Loader download fotos

--- photos/static/photos/js/configuracao.js
+++ photos/static/photos/js/configuracao.js
@@ -1,3 +1,10 @@
 $('#segundos_foto').on('input', (e) => {
-    $('#seg_span').text($('#segundos_foto').val())
+    $('#seg_span').text($('#segundos_foto').val());
+});
+
+$('#btn-salvar').on('click', (e)=>{
+    setTimeout(()=>{
+        $('#btn-salvar').hide();
+        $('#loader-imagens').show();
+    }, 2000);
 });
